b8eb55eaaeda48ddb7d4368322089383c649f729
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-11-09 17:33:05 +0500

    add
    addd

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
 console.log("hello");
+
+const sayHello = (name: any): void => {
+  console.log(`say hello ${name}`);
+};
+
+sayHello("leo");
+
+// Create our number formatter.
+var formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "UZS",
+
+  // These options are needed to round to whole numbers if that's what you want.
+  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+});
+
+const sum = formatter.format(2500); /* $2,500.00 */
+console.log(sum);
