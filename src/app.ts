a26e47ac22c302cc6a7195588c38153e6a41d68e
Afrosiyob <afrosiyob.a97@gmail.com>
2021-11-11 19:31:35 +0500

    add
    add

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,32 @@
 console.log("hello");
+
+enum color {
+    red = 1,
+    green = 4,
+    yellow = 3,
+    blue
+}
+
+
+console.log(color.red);
+console.log(color.green);
+console.log(color.yellow);
+console.log(color.blue);
+
+
+const msg: string = "this is error message"
+
+const errorFunc = (msg: string): never => {
+    throw new Error(msg)
+}
+
+// errorFunc(msg)
+
+type stum = string | number
+
+const fefe: stum = 3
+
+console.log(fefe);
+
+
+const createPassword = (name: string, password?: string | number) => `${name}${password}`
